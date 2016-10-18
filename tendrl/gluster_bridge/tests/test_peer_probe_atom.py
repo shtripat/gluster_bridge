49f4a7dd6352bd79ffc445fc1bcbf6f316852c3f
Shubhendu <shtripat@redhat.com>
2016-10-18 18:25:20 +0530

    Added missing test cases for peer management
    Signed-off-by: Shubhendu <shtripat@redhat.com>

---
+++ tendrl/gluster_bridge/tests/test_peer_probe_atom.py
@@ -0,0 +1,21 @@
+import mock
+import subprocess
+
+from tendrl.gluster_bridge.atoms.peer.probe import Probe
+
+
+class TestPeerProbe(object):
+
+    def test_start(object):
+        subprocess.call = mock.create_autospec(
+            subprocess.call,
+            return_value='done'
+        )
+        atom = Probe()
+        atom.start("node1")
+        subprocess.call.assert_called_with(
+            [
+                'gluster', 'peer', 'probe',
+                'node1', '--mode=script'
+            ]
+        )
